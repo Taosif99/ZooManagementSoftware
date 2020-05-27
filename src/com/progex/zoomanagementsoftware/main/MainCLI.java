0e7eec98211318b77bb9675891870ecacefde151
Mustapha Ouchen <mouchen1998@gmail.com>
2020-05-27 15:58:51 +0200

    Add packages

--- src/com/progex/zoomanagementsoftware/main/MainCLI.java
+++ src/com/progex/zoomanagementsoftware/main/MainCLI.java
@@ -11,6 +11,7 @@ public class MainCLI { //Main command line interface
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         System.out.println("Here everything begins !");
     }
     
