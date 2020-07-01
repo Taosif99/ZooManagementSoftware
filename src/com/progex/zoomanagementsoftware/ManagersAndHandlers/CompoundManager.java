0c7101a4341d468418ef426e607b73b341159812
Mustapha Ouchen <mouchen1998@gmail.com>
2020-07-01 13:33:05 +0200

    Removed unused imports in CompoundManager

--- src/com/progex/zoomanagementsoftware/ManagersAndHandlers/CompoundManager.java
+++ src/com/progex/zoomanagementsoftware/ManagersAndHandlers/CompoundManager.java
@@ -5,8 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  * Class which handles the compounds in our program.
