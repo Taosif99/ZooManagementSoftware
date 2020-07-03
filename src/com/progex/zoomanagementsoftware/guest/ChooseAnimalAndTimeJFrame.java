2c2e0d20743d5a969f46718ff151baf9d4d4f8e1
Taosif99 <58309338+Taosif99@users.noreply.github.com>
2020-07-03 14:11:27 +0200

    Minor changes

--- src/com/progex/zoomanagementsoftware/guest/ChooseAnimalAndTimeJFrame.java
+++ src/com/progex/zoomanagementsoftware/guest/ChooseAnimalAndTimeJFrame.java
@@ -153,7 +153,7 @@ public class ChooseAnimalAndTimeJFrame extends javax.swing.JFrame {
         jLabelTime.setText("Verfügbare Fütterungszeiten :");
         jLabelTime.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
-        jComboTime.setFont(new java.awt.Font("Calibri", 0, 36)); // NOI18N
+        jComboTime.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
         jComboTime.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         jComboTime.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jComboTime.addActionListener(new java.awt.event.ActionListener() {
@@ -170,7 +170,7 @@ public class ChooseAnimalAndTimeJFrame extends javax.swing.JFrame {
         jLabelAnimal.setFont(new java.awt.Font("Calibri", 0, 36)); // NOI18N
         jLabelAnimal.setText("Tiername :");
 
-        jComboBoxName.setFont(new java.awt.Font("Calibri", 0, 36)); // NOI18N
+        jComboBoxName.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
         jComboBoxName.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         jComboBoxName.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -231,7 +231,7 @@ public class ChooseAnimalAndTimeJFrame extends javax.swing.JFrame {
                 .addComponent(jLabelFindOut, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabelChoose)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 42, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 45, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabelAnimal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jComboBoxName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -239,9 +239,9 @@ public class ChooseAnimalAndTimeJFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabelTime, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jComboTime, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 43, Short.MAX_VALUE)
                 .addComponent(jButtonSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addContainerGap(21, Short.MAX_VALUE))
         );
 
         jLabelTime.getAccessibleContext().setAccessibleName("");
