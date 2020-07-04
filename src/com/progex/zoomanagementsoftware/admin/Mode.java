29b8a5e217ceb28912a751c96eccb01af94c5c21
Taosif99 <58309338+Taosif99@users.noreply.github.com>
2020-07-05 01:34:03 +0200

    Format code in package admin

--- src/com/progex/zoomanagementsoftware/admin/Mode.java
+++ src/com/progex/zoomanagementsoftware/admin/Mode.java
@@ -1,10 +1,9 @@
 package com.progex.zoomanagementsoftware.admin;
 
 /**
- *Enumeration to differ between the different modes which may appear
- * in a JFrame.
- *
+ * Enumeration to differ between the different modes which may appear in a
+ * JFrame.
  */
 public enum Mode {
- add,update,delete,admin,zookeeper    
+    add, update, delete, admin, zookeeper
 }
